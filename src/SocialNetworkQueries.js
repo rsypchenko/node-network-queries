0a99db647ac4271835e237bc4dfd2a248fffeb83
imhotep <r.sipchenko@gmail.com>
2022-12-17 22:53:25 -0700

    updates

--- src/SocialNetworkQueries.js
+++ src/SocialNetworkQueries.js
@@ -1,58 +1,56 @@
 export class SocialNetworkQueries {
+  constructor({ fetchCurrentUser }) {
+    this.user = null;
+    this.fetchCurrentUser = fetchCurrentUser;
+  }
 
-    constructor({ fetchCurrentUser }) {
-        this.fetchCurrentUser = fetchCurrentUser;
+  getListOfLikedBooks(user, minimalScore = 0) {
+    const books = [];
+    const map = {};
+    const { likes = [], friends = [] } = user;
+    const excluded = likes.books ?? [];
+
+    for (let i = 0; i < friends.length; i++) {
+      let books = friends[i]?.likes?.books?.filter(name => !excluded.includes(name));
+
+      for (let i = 0; i < books.length; i++) {
+        let book = books[i];
+        map[book] = map[book] ? map[book] + 1 : 1;
+      }
     }
 
-    async findPotentialLikes({ minimalScore } = {}) {
-        const books = [];
-        let user;
+    const keys = Object.keys(map).sort((a, b) => {
+      if (map[a] > map[b]) {
+        return -1;
+      } else if (map[a] < map[b]) {
+        return 1;
+      } else {
+        if (a > b) return 1;
+        else if (a < b) return -1;
+        else return 0;
+      }
+    });
 
-        try {
-            user = await this.fetchCurrentUser();
-        } catch (e) {
-            return Promise.resolve({ books });
-        }
+    for (let key of keys) {
+      let num = map[key] / friends.length;
+      if (num >= minimalScore) {
+        books.push(key);
+      }
+    }
 
-        const { likes = [], friends = [] } = user;
-        const excluded = likes.books ?? [];
+    return books;
+  }
 
-        const map = friends.reduce((acc, current) => {
-            let books = current?.likes?.books.filter(book => !excluded.includes(book));
-
-            for (let i = 0; i < books.length; i++) {
-                let book = books[i];
-                if (acc.hasOwnProperty(book)) {
-                    acc[book] = acc[book] + 1;
-                } else {
-                    acc[book] = 1;
-                }
-            }
-
-            return acc;
-        }, {});
-
-        const keys = Object.keys(map).sort((a, b) => {
-            if (map[a] > map[b]) {
-                return -1;
-            } else if (map[a] < map[b]) {
-                return 1;
-            } else {
-                if (a > b) return 1;
-                else if (a < b) return -1;
-                else return 0;
-            }
+  findPotentialLikes({ minimalScore } = {}) {
+    return new Promise((resolve) => {
+      this.fetchCurrentUser()
+        .then((user) => {
+          const books = this.getListOfLikedBooks(user, minimalScore);
+          resolve({ books });
+        })
+        .catch(() => {
+          resolve({ books: [] });
         });
-
-        for (let key of keys) {
-            let num = map[key] / friends.length;
-            if (num >= minimalScore) {
-                books.push(key);
-            }
-        }
-
-
-        return Promise.resolve({ books });
-    }
-
+    });
+  }
 }
